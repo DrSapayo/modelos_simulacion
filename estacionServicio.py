a2e9445d39048ccb5863c03cf72a1d8063837887
Mr. Sapayo <Julianccs27@gmail.com>
2025-02-03 17:27:11 -0500

    Update estacionServicio.py

--- estacionServicio.py
+++ estacionServicio.py
@@ -6,18 +6,34 @@ TIEMPO_LLEGADA = 5
 TIEMPO_SERVICIO = 7
 TIEMPO_ABIERTO = 600
 
+#variables para graficas
 tiempos = []
 vehiculos_en_sistema = []
 
+#variable contadora
+vehiculos_esperados = 0
+
 class Estacion:
-    def __init__(self, env, num_surtidores):
+    def __init__(self, env, num_surtidores,):
         self.env = env
         self.surtidores = simpy.Resource(env, num_surtidores)
 
     def atencion_vehiculo(self, vehiculo):
+        global vehiculos_esperados
         tiempo = random.expovariate(1.0/TIEMPO_SERVICIO)
         yield self.env.timeout(tiempo)
         print(f'El vehiculo {vehiculo} atendido en {self.env.now:.2f}')
+        vehiculos_esperados += 1
+        registro_vehiculos(env.now, vehiculos_esperados)
+
+        with self.surtidores.request() as req:
+            yield req
+            print(f'Vehiculo {vehiculo} esta siendo atendido en el minuto {self.env.now:.2f}')
+            vehiculos_esperados -= 1
+            registro_vehiculos(env.now, vehiculos_esperados)
+            yield self.env.timeout(tiempo)
+            print(f'Vehiculo {vehiculo} termino su servicio en el minuto {self.env.now:.2f}')
+
 
 def llegada_vehiculos(env, estacion):
     vehiculo_id = 0
@@ -26,6 +42,10 @@ def llegada_vehiculos(env, estacion):
         vehiculo_id += 1
         print(f'Vehiculo {vehiculo_id} llega a la estacion de servicio a los {env.now:.2f}')
         env.process(estacion.atencion_vehiculo(vehiculo_id))
+
+def registro_vehiculos(tiempo, vehiculos):
+    tiempos.append(tiempo)
+    vehiculos_en_sistema.append(vehiculos)
 
 env = simpy.Environment()
 estacion = Estacion(env, num_surtidores=2)
